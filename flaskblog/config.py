b064ec45541b748ab44c5f686ad8a09e8cf49c94
Dmitry <dmitrix_n@mail.ru>
2022-10-24 19:43:51 +0300

    added config.json

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,12 +1,15 @@
-import os
+import json
+
+with open("/etc/config.json") as config_file:
+    config = json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = config.get('SECRET_KEY')
     DB_NAME = "site.db"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_NAME
     MAIL_SERVER = 'smtp.mail.ru'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USERNAME = "dmitrix_n@mail.ru" 
-    MAIL_PASSWORD = "b3mLFnxNrxRfKGA2uqfw"
+    MAIL_PASSWORD = config.get("MAIL_PASSWORD")
